27bda20717560beede3b16e3ffabd9b2e069b283
Amir Daneshmand <amirdanesh02@gmail.com>
2025-04-17 02:40:47 +0330

    feat(user/routes): add API endpoints responisve of auth-relative requests (GET, POST, PATCH).

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+import AuthController from "../controllers/auth.controller";
+
+export const authRoutes = (router: Router): void => {
+  const baseURL: string = "/auth/";
+  router.post(baseURL + "register", AuthController.register);
+  router.post(baseURL + "login", AuthController.login);
+  router.get(baseURL + "verify-email/:token", AuthController.verifyEmail);
+  router.patch(
+    baseURL + "to-reset-password",
+    AuthController.resetPasswordRequest
+  );
+  router.patch(baseURL + "reset-password/:token", AuthController.resetPassword);
+};
