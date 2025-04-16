f41b2254f20197843ed8ba4dfd39abf8cb65af17
Amir Daneshmand <amirdanesh02@gmail.com>
2025-04-17 01:31:01 +0330

    feat(user/repository): implement MongoDB adapter for user persistence

---
+++ server/src/repositories/user.repo.ts
@@ -0,0 +1,51 @@
+import { ObjectId } from "mongodb";
+import { injectable, inject } from "inversify";
+
+import UserModel from "../models/user.model";
+import { User, Users, CreateUserDTO, UpdateUserDTO } from "../types/user.type";
+
+type AsyncUser = Promise<User | null>;
+
+@injectable()
+class UserRepository {
+  constructor(@inject(UserModel) private readonly model: UserModel) {}
+
+  public async getAllUsers(): Promise<Users> {
+    return this.model.model.find();
+  }
+
+  public async getUserById(id: ObjectId): AsyncUser {
+    return this.model.model.findById(id);
+  }
+
+  public async getUserByEmail(email: string): AsyncUser {
+    return this.model.model.findOne({ "auth.email": email });
+  }
+
+  public async createNewUser(data: CreateUserDTO): Promise<User> {
+    return new this.model.model(data);
+  }
+
+  public async updateUserById(id: ObjectId, data: UpdateUserDTO): AsyncUser {
+    return this.model.model.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  public async updateUserByEmail(
+    email: string,
+    data: UpdateUserDTO
+  ): AsyncUser {
+    return this.model.model.findOneAndUpdate({ "auth.email": email }, data, {
+      new: true,
+    });
+  }
+
+  public async deleteUserById(id: ObjectId): AsyncUser {
+    return this.model.model.findByIdAndDelete(id);
+  }
+
+  public async deleteUserByEmail(email: string): AsyncUser {
+    return this.model.model.findOneAndDelete({ "auth.email": email });
+  }
+}
+
+export default UserRepository;
