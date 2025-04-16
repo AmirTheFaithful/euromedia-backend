989e8dff069f7b08e2190e95fb5752a4c9b02ee5
Amir Daneshmand <amirdanesh02@gmail.com>
2025-04-17 02:11:13 +0330

    feat(user/controller): add controller layer responsive of managing API users-related requests.

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+
+import UserContainer from "../containers/user-di";
+import { asyncHandler } from "../utils/asyncHandler";
+import { User, Users } from "../types/user.type";
+import {
+  FetchUserUseCase,
+  UpdateUserUseCase,
+  DeleteUserUseCase,
+  Queries,
+  Body,
+} from "../use-cases/user.use-case";
+
+type ResponseBody<DataType> = {
+  data: DataType;
+  message: string;
+};
+
+class UserController {
+  public getUsers = asyncHandler(
+    async (
+      req: Request<any, User | User, any, Queries>,
+      res: Response<ResponseBody<User | Users>>
+    ) => {
+      const fetchUserUseCase = UserContainer.get(FetchUserUseCase);
+      const data: User | Users = await fetchUserUseCase.execute(req.query);
+      res.status(200).json({ data, message: "Fetch success." }).end();
+    }
+  );
+
+  public updateUser = asyncHandler(
+    async (
+      req: Request<any, User, Body, Queries>,
+      res: Response<ResponseBody<User>>
+    ) => {
+      const updateUserUseCase = UserContainer.get(UpdateUserUseCase);
+      const data: User = await updateUserUseCase.execute(req.query, req.body);
+      res.status(200).json({ data, message: "Update success." }).end();
+    }
+  );
+
+  public deleteUser = asyncHandler(async (req: Request, res: Response) => {
+    const deleteUserUseCase = UserContainer.get(DeleteUserUseCase);
+    const data: User = await deleteUserUseCase.execute(req.query);
+    res.status(200).json({ data, message: "Deletion success." }).end();
+  });
+}
+
+export default new UserController();
