9e58dd720a97cf25aedd1c01c7466a65d2accb71
Amir Daneshmand <amirdanesh02@gmail.com>
2025-04-30 03:55:06 +0330

    feat(routes): include posts routes to centrall routes 'hub'.

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import { userRoutes } from "./user.routes";
 import { authRoutes } from "./auth.routes";
+import { postRoutes } from "./post.routes";
 
 const router: Router = Router();
 
 export const centralRouter = (): Router => {
   userRoutes(router);
   authRoutes(router);
+  postRoutes(router);
 
   return router;
 };
