ce3344a3a62588dd51b6f379c1865beeba1f442b
Amir Daneshmand <amirdanesh02@gmail.com>
2025-04-30 17:34:25 +0330

    feat(container/post): include 'UpdatePostUseCase' and 'DeletePostUseCase' classes to the posts dependency injection container.

--- server/src/containers/post.container.ts
+++ server/src/containers/post.container.ts
@@ -3,9 +3,13 @@ import { Container } from "inversify";
 import {
   FetchPostUseCase,
   CreatePostUseCase,
+  UpdatePostUseCase,
+  DeletePostUseCase,
 } from "../use-cases/post.use-case";
 
 export default (container: Container): void => {
   container.bind(FetchPostUseCase).toSelf();
   container.bind(CreatePostUseCase).toSelf();
+  container.bind(UpdatePostUseCase).toSelf();
+  container.bind(DeletePostUseCase).toSelf();
 };
