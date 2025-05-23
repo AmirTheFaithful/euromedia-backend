b908b7f95edca0bb4ab54f96f131114ea04be75d
Amir Daneshmand <amirdanesh02@gmail.com>
2025-05-23 16:42:10 +0330

    feat(container/post): bind PostModel, PostRepository, and PostService to container.

--- server/src/containers/post.container.ts
+++ server/src/containers/post.container.ts
@@ -1,5 +1,9 @@
 import { Container } from "inversify";
 
+import PostModel from "../models/post.model";
+import PostRepository from "../repositories/post.repo";
+import PostService from "../services/post.service";
+
 import {
   FetchPostUseCase,
   CreatePostUseCase,
@@ -8,6 +12,10 @@ import {
 } from "../use-cases/post.use-case";
 
 export default (container: Container): void => {
+  container.bind(PostModel).toSelf();
+  container.bind(PostRepository).toSelf();
+  container.bind(PostService).toSelf();
+
   container.bind(FetchPostUseCase).toSelf();
   container.bind(CreatePostUseCase).toSelf();
   container.bind(UpdatePostUseCase).toSelf();
