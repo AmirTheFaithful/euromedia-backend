fef81b60bf08e76b1166b66bf7a68b45042549a5
Amir Daneshmand <amirdanesh02@gmail.com>
2025-05-23 16:49:41 +0330

    feat(server/container): register comment DI container in the central container.

--- server/src/containers/index.ts
+++ server/src/containers/index.ts
@@ -4,6 +4,7 @@ import userContainer from "./user.container";
 import authContainer from "./auth.container";
 import postContainer from "./post.container";
 import reactionContainer from "./reaction.container";
+import commentContainer from "./comment.container";
 
 export default (): Container => {
   const centralContainer: Container = new Container();
@@ -12,6 +13,7 @@ export default (): Container => {
   authContainer(centralContainer);
   postContainer(centralContainer);
   reactionContainer(centralContainer);
+  commentContainer(centralContainer);
 
   return centralContainer;
 };
