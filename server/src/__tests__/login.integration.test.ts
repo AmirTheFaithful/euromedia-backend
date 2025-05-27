4113e043dabddb32f3f44470fa621878ededd6b3
Amir Daneshmand <amirdanesh02@gmail.com>
2025-05-27 03:42:56 +0330

    refactor(integration-test/login): replace single dummy user by multiple for greater confidence in the sustainability of conflicts.

--- server/src/__tests__/login.integration.test.ts
+++ server/src/__tests__/login.integration.test.ts
@@ -1,44 +1,33 @@
-import { connect, connection } from "mongoose";
 import request, { Response } from "supertest";
 import { Application } from "express";
 
 import app from "../setup/app";
-import UserModel from "../models/user.model";
-import { mongo } from "../config/env";
 
 let testApp: Application;
 
 const baseURL = "/auth/login";
-const testModel = new UserModel().model;
-const dummyUser = {
-  firstname: "John",
-  lastname: "Doe",
-  email: "legoviking8@gmail.com",
-  password: "just_doe_it!",
-};
 
-beforeAll(async (): Promise<void> => {
-  await connect(mongo.test_uri);
-  await testModel.deleteMany();
-
+beforeAll(async () => {
   testApp = app();
-});
-
-afterAll(async (): Promise<void> => {
-  await testModel.deleteMany();
-  await connection.close();
 });
 
 jest.setTimeout(16000); // Increase testing estimated timeout.
 
 describe("POST auth/login", () => {
   it("Logs in an registered user.", async () => {
+    const dummyUser = {
+      firstname: "John",
+      lastname: "Doe",
+      email: "john_doe_LA2018@oakland.ca",
+      password: "just_doe_it!",
+    };
+
     // First, register a user.
     await request(testApp).post("/auth/register").send(dummyUser);
 
     const loginCredentials = {
-      email: "legoviking8@gmail.com",
-      password: "just_doe_it!",
+      email: dummyUser.email,
+      password: dummyUser.password,
     };
 
     // Now, test the login process itself.
@@ -51,12 +40,19 @@ describe("POST auth/login", () => {
   });
 
   it("Refuses to log in a user with wrong password", async () => {
+    const dummyUser = {
+      firstname: "Jane",
+      lastname: "Doe",
+      email: "jane092718@gmail.com",
+      password: "91283Los-Angeles",
+    };
+
     // First, register a user.
     await request(testApp).post("/auth/register").send(dummyUser);
 
     const loginCredentials = {
-      email: "legoviking8@gmail.com",
-      password: "some-incorrect-password",
+      email: dummyUser.email,
+      password: dummyUser.password + "abc",
     };
 
     const loginResponse: Response = await request(testApp)
@@ -70,7 +66,7 @@ describe("POST auth/login", () => {
   it("Refuses to log in a user with not existing email", async () => {
     const loginCredentials = {
       email: "unexisting-dummy-email@gmail.com",
-      password: "just_doe_it!",
+      password: "19123891249",
     };
 
     const loginResponse: Response = await request(testApp)
