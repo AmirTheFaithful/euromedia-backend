97b50887af7b10e124293b5562b49f095ec7bae0
Amir Daneshmand <amirdanesh02@gmail.com>
2025-06-03 17:58:22 +0330

    feat(route/comment): register 'updateComment' metod route.

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -7,4 +7,5 @@ const baseURL: string = "/api/comments";
 export const commentRoutes = (router: Router): void => {
   router.get(baseURL, commentsController.getComments);
   router.post(baseURL, commentsController.createComment);
+  router.patch(baseURL, commentsController.updateComment);
 };
