a8c1fd16b1680823f8cf36a56dbfa9166c08a86d
Amir Daneshmand <amirdanesh02@gmail.com>
2025-08-29 01:14:28 +0330

    refactor(client): Completely remove client folder from the project

--- client/jest.config.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "jsdom", // To test React components
-  transform: {
-    "^.+\\.[tj]sx?$": "babel-jest", // Використовуємо babel для трансформації tsx
-  },
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
-};
