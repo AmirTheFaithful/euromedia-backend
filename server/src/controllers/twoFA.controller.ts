db190b633404e6e7d6c5c85238899f4238c03f5f
Amir Daneshmand <amirdanesh02@gmail.com>
2025-09-08 03:27:26 +0330

    feat(controllers/2fa): impplement verify 2fa controller

--- server/src/controllers/twoFA.controller.ts
+++ server/src/controllers/twoFA.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 import Container from "../containers";
 import { asyncHandler } from "../utils/asyncHandler";
-import { Setup2FAUseCase } from "../use-cases/twoFAUseCase";
+import { Setup2FAUseCase, Verify2FAUseCase } from "../use-cases/twoFAUseCase";
 
 class TwoFAController {
   constructor(private readonly container = Container()) {}
@@ -16,6 +16,16 @@ class TwoFAController {
       otpAuthURL: payload.otpAuthURL,
       message: "2FA setup success.",
     });
+  });
+
+  public verify = asyncHandler(async (req: Request, res: Response) => {
+    const usecase = this.container.get(Verify2FAUseCase);
+    const { accessToken, refreshToken } = await usecase.execute(
+      req.headers["authorization"],
+      req.body.twoFACode
+    );
+    res.cookie("refresh-token", refreshToken);
+    res.status(200).json({ accessToken });
   });
 }
 
