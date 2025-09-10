1fc51e6a1ac0b0b5d453169eea2eb366786a072e
Amir Daneshmand <amirdanesh02@gmail.com>
2025-09-11 03:02:46 +0330

    feat(containers/2fa): Bind Initiate2FAUseCase use-case to 2FA DI container

--- server/src/containers/twoFA.container.ts
+++ server/src/containers/twoFA.container.ts
@@ -1,8 +1,13 @@
 import { Container } from "inversify";
 
-import { Setup2FAUseCase, Verify2FAUseCase } from "../use-cases/twoFAUseCase";
+import {
+  Setup2FAUseCase,
+  Verify2FAUseCase,
+  Initiate2FAUseCase,
+} from "../use-cases/twoFAUseCase";
 
 export default (container: Container): void => {
   container.bind(Setup2FAUseCase).toSelf();
   container.bind(Verify2FAUseCase).toSelf();
+  container.bind(Initiate2FAUseCase).toSelf();
 };
