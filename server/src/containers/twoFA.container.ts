2f6240fa92b380115f20346747ca634696b91c29
Amir Daneshmand <amirdanesh02@gmail.com>
2025-09-20 17:43:36 +0330

    feat(containers/2fa): Bind Deinit2FAUseCase use-case to 2FA DI container

--- server/src/containers/twoFA.container.ts
+++ server/src/containers/twoFA.container.ts
@@ -4,10 +4,12 @@ import {
   Setup2FAUseCase,
   Verify2FAUseCase,
   Initiate2FAUseCase,
+  Deinit2FAUseCase,
 } from "../use-cases/twoFAUseCase";
 
 export default (container: Container): void => {
   container.bind(Setup2FAUseCase).toSelf();
   container.bind(Verify2FAUseCase).toSelf();
   container.bind(Initiate2FAUseCase).toSelf();
+  container.bind(Deinit2FAUseCase).toSelf();
 };
